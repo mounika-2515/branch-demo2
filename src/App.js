72e5d25bd9c1b3e5270623a501478fa7f369cfe4
mounika-2515 <mounikayarram32@gmail.com>
2024-07-26 16:25:54 +0530

    commit 1st

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Attendance from "./Attendance";
+import Classes from "./Classes";
+
+function App(){
+  return(
+    <>
+      <h>
+        branch-demo1
+        <Classes />
+        <Attendance />
+      </h>
+    </>
+  );
+}
+export default App;
